2d073730b8c6212aa66fa0710bf092828fa2eb10
loliksamuel <o528060707@gmail.com>
2017-03-28 20:53:42 +0300

    hazaha

--- src/_MainWindow.java
+++ src/_MainWindow.java
@@ -48,16 +48,16 @@ class _MainWindow extends Frame implements ActionListener {
         buttonRt.setLabel(">");
         buttonDn.setLabel("\\/");
 
-        buttonSwitch.setBounds(20, 30, 100, 30);
-        buttonRestartGame.setBounds(130, 30, 120, 30);
-        buttonUp.setBounds(80, 100, 30, 30);// setting button position
-        buttonLt.setBounds(20 , 130, 30, 30);// setting button position
-        buttonRt.setBounds(140, 130, 30, 30);// setting button position
-        buttonDn.setBounds(80 , 160, 30, 30);// setting button position
+        buttonSwitch.setBounds      ( 20, 30, 100, 30);
+        buttonRestartGame.setBounds (130, 30, 120, 30);
+        buttonUp.setBounds          (80 , 100, 30, 30);// setting button position
+        buttonLt.setBounds          (20 , 130, 30, 30);// setting button position
+        buttonRt.setBounds          (140, 130, 30, 30);// setting button position
+        buttonDn.setBounds          (80 , 160, 30, 30);// setting button position
 
-        lblGameCurr.setBounds(20 , 60, 100, 30);// setting button position
-        lblGameCurrr.setBounds(130 , 60, 120, 30);// setting button position
-        lblStatus.setBounds(20 , 300, 500, 30);// setting button position
+        lblGameCurr.setBounds       ( 20 , 60 , 100, 30);// setting button position
+        lblGameCurrr.setBounds      (130 , 60 , 120, 30);// setting button position
+        lblStatus.setBounds         ( 20 , 300, 500, 30);// setting button position
 
         setSize(270, 400);//frame size 300 width and 300 height
         setLayout(null);//no layout manager
